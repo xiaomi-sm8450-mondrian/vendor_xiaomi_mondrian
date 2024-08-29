166b223c2f2db85134625404d5c35a16416c0252
Arian <arian.kulmer@web.de>
2024-08-29 20:48:29 +0200

    mondrian: Move sensors hals.conf back to vendor repo
    sensors.xiaomi is added by common tree now
    
    Change-Id: I541b656304bfde478966c84115d94bc989742149

--- mondrian-vendor.mk
+++ mondrian-vendor.mk
@@ -502,6 +502,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/mondrian/proprietary/vendor/etc/sensors/config/sns_wrist_pedo.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/sns_wrist_pedo.json \
     vendor/xiaomi/mondrian/proprietary/vendor/etc/sensors/config/stk3bcx_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/stk3bcx_0.json \
     vendor/xiaomi/mondrian/proprietary/vendor/etc/sensors/config/sx933x_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/sx933x_0.json \
+    vendor/xiaomi/mondrian/proprietary/vendor/etc/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf \
     vendor/xiaomi/mondrian/proprietary/vendor/etc/sensors/judge_calibrated.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/judge_calibrated.json \
     vendor/xiaomi/mondrian/proprietary/vendor/etc/sensors/sns_reg_config:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sns_reg_config \
     vendor/xiaomi/mondrian/proprietary/vendor/etc/thermal-4k.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-4k.conf \
