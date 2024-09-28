1a0d621fbb85f50c57ad2c3d427698ec343180db
FlowerSea0208 <zzp060208@gmail.com>
2024-09-29 00:58:45 +0800

    mondrian: Switch to source-built fx.tunnel interface
    Change-Id: I1b96a1df3758106b7516ee7b9ac90bd9374f6d1c

--- mondrian-vendor.mk
+++ mondrian-vendor.mk
@@ -1120,7 +1120,6 @@ PRODUCT_PACKAGES += \
     misound_res_spk \
     misound_res_spk4ch \
     weak_refs \
-    vendor.xiaomi.hardware.fx.tunnel@1.0 \
     vendor.xiaomi.hardware.quickcamera@1.0-service.xml \
     batterysecret \
     vendor.qti.camera.provider@2.7-service_64 \
